b72cd05d6c49c64fc286d4ab937aeebafdbe564b
Yang ye lin <oky07031217@gmail.com>
2023-05-13 15:39:29 +0900

    Add my_page.js

---
+++ js/vitamin_hyatt/loader.js
@@ -0,0 +1,31 @@
+async function injectNavbar() {
+    fetch("/vitamin_hyatt/navbar.html").then(response => {
+        return response.text()
+    })
+        .then(data => {
+            document.querySelector("header").innerHTML = data;
+        })
+
+    let navbarHtml = await fetch("/vitamin_hyatt/navbar.html")
+    let data = await navbarHtml.text()
+    document.querySelector("header").innerHTML = data;
+
+    const payload = localStorage.getItem("payload");
+    if (payload) {
+        const payload_parse = JSON.parse(payload)
+
+        const mypage = document.getElementById("mypage")
+        mypage.innerText = "마이페이지"
+
+        const logout = document.getElementById("logout")
+        logout.innerText = "로그아웃"
+
+        let loginButton = document.getElementById("login-button")
+        loginButton.style.display = "none";
+
+        let signupButton = document.getElementById("signup-button")
+        signupButton.style.display = "none";
+    }
+}
+
+injectNavbar();
---
+++ js/vitamin_hyatt/my_page.js
@@ -0,0 +1,22 @@
+async function loadUserprofile() {
+
+    const response = await getUserprofile();
+
+    const username = document.getElementById("username")
+    username.innerText = response.profile.username
+
+    const email = document.getElementById("email")
+    email.innerText = response.profile.email
+
+    const phone = document.getElementById("phone")
+    phone.innerText = response.profile.phone
+
+    const point = document.getElementById("point")
+    point.innerText = response.profile.point
+
+
+}
+
+window.onload = async function () {
+    await loadUserprofile();
+}

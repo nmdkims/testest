519ca37dad06c8ec6bf269ba3dec9032281edb69
miyeong <kmy9810@naver.com>
2023-05-13 19:51:39 +0900

    Add GetRoom()

--- js/book_user_list.js
+++ js/book_user_list.js
@@ -1,16 +1,41 @@
 window.onload = () => {
-    bookUserList()
+    GetRoom()
 }
 
-async function bookUserList() {
-    const response = await fetch('http://127.0.0.1:8000/manager/customers/1/', {
+async function GetRoom() {
+    const accessToken = localStorage.getItem('access')
+    const response = await fetch(`http://127.0.0.1:8000/manager/rooms/`, {
         headers: {
             'content-type': 'application/json',
+            'Authorization': `Bearer ${accessToken}`,
+        },
+        method: 'GET',
+    })
+    const response_json = await response.json()
+    response_json.forEach((a) => {
+        let id = a['id']
+        let name = a['name']
+        let temp_html = `<option value="${id}">${name}</option>`
+        $('#select_room').append(temp_html)
+    })
+    // 
+}
+
+
+
+async function bookUserList() {
+    const accessToken = localStorage.getItem('access')
+    const room_id = document.getElementById("select_room").value
+    const response = await fetch(`http://127.0.0.1:8000/manager/customers/${room_id}/`, {
+        headers: {
+            'content-type': 'application/json',
+            'Authorization': `Bearer ${accessToken}`,
         },
         method: 'GET',
     })
 
     const response_json = await response.json()
+    console.log(response_json)
 
     response_json['book_set'].forEach((a) => {
         const num = a['id']

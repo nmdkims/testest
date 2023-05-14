36952177e790efb3f761b37c3f01df350ffc6433
KIMHOONHEE <89897944+nmdkims@users.noreply.github.com>
2023-05-15 04:47:13 +0900

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,5 +1,5 @@
 // 기본 URL
-const backend_base_url = "http://127.0.0.1:8000"
+const backend_base_url = "https://nmdkims.com"
 const frontend_base_url = "http://127.0.0.1:5500"
 
 window.onload = () => {
